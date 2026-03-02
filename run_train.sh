b2368699cca6b6b6a4deabddadc63ccca930b9b5
Haochen Zhang <hz112@rice.edu>
2026-03-02 13:08:17 -0600

    init

---
+++ run_train.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=6 python run_caption_laura.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/train_images/train/hatefulMemes" --split "train_hatefulMemes"
+
