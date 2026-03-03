093113ff6c4f4b321627223aac963d62346f31d1
Haochen Zhang <hz112@rice.edu>
2026-03-03 10:13:34 -0600

    0303

---
+++ run_both.sh
@@ -0,0 +1,2 @@
+bash run_test.sh
+bash run_train.sh
--- run_test.sh
+++ run_test.sh
@@ -1,7 +1,13 @@
 #CUDA_VISIBLE_DEVICES=7 python run_caption_laura.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/hatefulMemes" --split "test_hatefulMemes"
 #CUDA_VISIBLE_DEVICES=7 python run_caption_laura.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/bad_ads" --split "test_bad_ads"
 #CUDA_VISIBLE_DEVICES=7 python run_caption_laura.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/harm-p" --split "test_harm-p"
-CUDA_VISIBLE_DEVICES=7 python run_caption_laura.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/HOD" --split "test_HOD"
-CUDA_VISIBLE_DEVICES=7 python run_caption_laura.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/privacyAlert" --split "test_privacyAlert"
+#CUDA_VISIBLE_DEVICES=7 python run_caption_laura.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/HOD" --split "test_HOD"
+#CUDA_VISIBLE_DEVICES=7 python run_caption_laura.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/privacyAlert" --split "test_privacyAlert"
+#
 
+CUDA_VISIBLE_DEVICES=6 python run_caption_laura_one_sentence.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/hatefulMemes" --split "test_hatefulMemes"
+CUDA_VISIBLE_DEVICES=6 python run_caption_laura_one_sentence.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/bad_ads" --split "test_bad_ads"
+CUDA_VISIBLE_DEVICES=6 python run_caption_laura_one_sentence.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/harm-p" --split "test_harm-p"
+CUDA_VISIBLE_DEVICES=6 python run_caption_laura_one_sentence.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/HOD" --split "test_HOD"
+CUDA_VISIBLE_DEVICES=6 python run_caption_laura_one_sentence.py --part_id 0 --num_parts 1  --image_folder "/playpen-shared/laura/unlearning/VLGuard/test_images/test/privacyAlert" --split "test_privacyAlert"
 
