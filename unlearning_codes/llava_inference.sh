0d6e19a4e5e19ea22244d70935de89cf3ade5854
Haochen Zhang <hz112@rice.edu>
2026-03-19 09:59:25 -0500

    0318

--- unlearning_codes/llava_inference.sh
+++ unlearning_codes/llava_inference.sh
@@ -5,21 +5,21 @@ export HF_DATASETS_CACHE=$HF_HOME
 export CUDA_VISIBLE_DEVICES=6
 
 
-#python llava_inference.py \
-#  --base_model llava-hf/llava-1.5-7b-hf \
-#  --model_path ./ckpt/graddiff_mm \
-#  --input_json /playpen-shared/laura/unlearning/safety/vlguard_test_qwen_input.json \
-#  --image_root /playpen-shared/laura/unlearning/VLGuard/test_images/test \
-#  --mode multimodal \
-#  --output_name tuned_multimodal.json
+python llava_inference.py \
+  --base_model llava-hf/llava-1.5-7b-hf \
+  --model_path ./ckpt/graddiff_mm \
+  --input_json /playpen-shared/laura/unlearning/safety/vlguard_test_qwen_input.json \
+  --image_root /playpen-shared/laura/unlearning/VLGuard/test_images/test \
+  --mode multimodal \
+  --output_name tuned_multimodal.json
 
 
-#python llava_inference.py \
-#  --base_model llava-hf/llava-1.5-7b-hf \
-#  --model_path ./ckpt/graddiff_text \
-#  --input_json /playpen-shared/laura/unlearning/safety/vlguard_test_qwen_input_text_only.json \
-#  --mode text_only \
-#  --output_name tuned_text_only.json
+python llava_inference.py \
+  --base_model llava-hf/llava-1.5-7b-hf \
+  --model_path ./ckpt/graddiff_text \
+  --input_json /playpen-shared/laura/unlearning/safety/vlguard_test_qwen_input_text_only.json \
+  --mode text_only \
+  --output_name tuned_text_only.json
 
 
 
