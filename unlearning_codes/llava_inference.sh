7709bcaca788b0a629d87417f7db41dc79196ace
Haochen Zhang <hz112@rice.edu>
2026-03-19 09:47:24 -0500

    0318

--- unlearning_codes/llava_inference.sh
+++ unlearning_codes/llava_inference.sh
@@ -16,20 +16,20 @@ python llava_inference.py \
 
 #!/bin/bash
 
-python llava_inference.py \
-  --base_model llava-hf/llava-1.5-7b-hf \
-  --model_path ./ckpt/graddiff_text \
-  --input_json /playpen-shared/laura/unlearning/safety/vlguard_test_qwen_input_text_only.json \
-  --mode text_only \
-  --output_name text_only.json
-
-
-#!/bin/bash
-
-python llava_inference.py \
-  --base_model llava-hf/llava-1.5-7b-hf \
-  --model_path ./ckpt/graddiff_img \
-  --input_json /playpen-shared/laura/unlearning/safety/vlguard_test_qwen_captioning.json \
-  --image_root /playpen-shared/laura/unlearning/VLGuard/test_images/test \
-  --mode image_only \
-  --output_name image_only.json
+#python llava_inference.py \
+#  --base_model llava-hf/llava-1.5-7b-hf \
+#  --model_path ./ckpt/graddiff_text \
+#  --input_json /playpen-shared/laura/unlearning/safety/vlguard_test_qwen_input_text_only.json \
+#  --mode text_only \
+#  --output_name text_only.json
+#
+#
+##!/bin/bash
+#
+#python llava_inference.py \
+#  --base_model llava-hf/llava-1.5-7b-hf \
+#  --model_path ./ckpt/graddiff_img \
+#  --input_json /playpen-shared/laura/unlearning/safety/vlguard_test_qwen_captioning.json \
+#  --image_root /playpen-shared/laura/unlearning/VLGuard/test_images/test \
+#  --mode image_only \
+#  --output_name image_only.json
